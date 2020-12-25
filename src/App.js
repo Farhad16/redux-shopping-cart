1dd7ef773a837db720716a42e5f9ca1e190804a8
farhad@389 <fhossain389@.com>
2020-12-26 00:57:46 +0600

    Filter products done using redux

--- src/App.js
+++ src/App.js
@@ -45,55 +45,7 @@ class App extends React.Component {
 
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
   }
-  sortProducts = (event) => {
-    const sort = event.target.value;
-    this.setState(() => ({
-      sort: sort,
-      products: this.state.products
-        .slice()
-        .sort((a, b) =>
-          sort === "lowest"
-            ? a.price > b.price
-              ? 1
-              : -1
-            : sort === "highest"
-              ? a.price < b.price
-                ? 1
-                : -1
-              : a._id < b._id
-                ? 1
-                : -1
-        ),
-    }));
-  };
 
-  filterProducts = (event) => {
-    if (event.target.value === '') {
-      this.setState({ size: event.target.value, products: data.products })
-    } else {
-      this.setState({
-        size: event.target.value,
-        products: data.products.filter(product => product.availableSizes.indexOf(event.target.value) >= 0)
-      })
-    }
-  }
-
-  categoryProducts = (event) => {
-    console.log(event.target.value);
-    if (event.target.value === '') {
-      this.setState({
-        category: event.target.value,
-        products: data.products,
-      })
-    } else {
-      const pro = data.products.filter(product => product.category.indexOf(event.target.value) >= 0);
-      console.log(pro);
-      this.setState({
-        category: event.target.value,
-        products: pro,
-      })
-    }
-  }
 
   createOrder = (order) => {
     alert("Order should be save for " + order.name)
@@ -112,14 +64,7 @@ class App extends React.Component {
           <main>
             <div className="content">
               <div className="main">
-                <Filter count={this.state.products.length}
-                  size={this.state.size}
-                  sort={this.state.sort}
-                  category={this.state.category}
-                  filterProducts={this.filterProducts}
-                  sortProducts={this.sortProducts}
-                  categoryProducts={this.categoryProducts}
-                />
+                <Filter />
                 <div className="productSection">
                   <Products
                     addToCart={this.addToCart}
